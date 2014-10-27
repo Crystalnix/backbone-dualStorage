1134b49fafc1e91444152e3d4a5197323700519d
Alexey Shaposhnikov <shaposhnikov@crystalnix.com>
2014-10-27 19:23:07 +0600

    modified logic of saving in idb: added states and filter for 'getAll' method. added fullSync method

--- backbone.dualstorage.coffee
+++ backbone.dualstorage.coffee
@@ -4,6 +4,7 @@
     SYNCHRONIZED: 'SYNCHRONIZED'
     SYNCHRONIZING: 'SYNCHRONIZING'
     UPDATE_FAILED: 'UPDATE_FAILED'
+    CREATE_FAILED: 'CREATE_FAILED'
     DELETE_FAILED: 'DELETE_FAILED'
 
   eventNames =
@@ -23,8 +24,46 @@
   Backbone.DualModel = Backbone.Model.extend
     states: states,
 
+    remoteIdAttribute: 'id',
+
+    getUrlForSync: (urlRoot, method) ->
+      remoteId = @get(@remoteIdAttribute)
+      if remoteId and method in ['update', 'delete']
+        return "#{urlRoot}/#{remoteId}/"
+      return urlRoot
+
+    isInSynchronizing: () ->
+      @get('status') is @states.SYNCHRONIZING
+
     isDelayed: () ->
       @get('status') in [@states.DELETE_FAILED, @states.UPDATE_FAILED]
+
+  Backbone.IndexedDB.prototype.create = (model, options) ->
+    model.set('status', states.CREATE_FAILED)
+    data = model.attributes;
+    @store.put(data, (insertedId) =>
+      data[@keyPath] = insertedId;
+      options.success(data)
+    , options.error);
+
+  Backbone.IndexedDB.prototype.update = (model, options) ->
+    model.set('status', states.UPDATE_FAILED)
+    data = model.attributes
+    @store.put(data, options.success, options.error);
+
+  Backbone.IndexedDB.prototype.getAll = (options) ->
+    data = []
+    @iterate((item) ->
+      if item.status != states.DELETE_FAILED
+        data.push(item)
+    , onEnd: () ->options.success(data))
+
+  Backbone.IndexedDB.prototype.destroy = (model, options) ->
+    if model.isNew()
+      return false
+    model.set('status', states.DELETE_FAILED)
+    data = model.attributes
+    @store.put(data, options.success, options.error)
 
 
   Backbone.DualCollection = Backbone.Collection.extend
@@ -32,26 +71,43 @@
 
     eventNames: eventNames
 
+    getSyncMethodsByState: (state) ->
+      method = switch
+        when @states.CREATE_FAILED is state then 'create'
+        when @states.UPDATE_FAILED is state then 'update'
+        when @states.DELETE_FAILED is state then 'delete'
+
     merge: (newData) ->
       newData
 
     firstSync: (options = {}) ->
       originalSuccess = options.success or $.noop
-
+      event = _.extend {}, Backbone.Events
       syncSuccess = (response) =>
         data = @merge(@parse(response))
+        event.trigger(@eventNames.REMOTE_SYNC_SUCCESS)
         method = if options.reset then 'reset' else 'set';
         @[method](data, options);
         originalSuccess(@, data, options)
         @trigger('sync', @, data, options);
         wrapError(@, options)
 
-      fetchSuccess = () =>
+      syncError = (error) =>
+        event.trigger(@eventNames.REMOTE_SYNC_FAIL, error, options)
+
+
+      fetchSuccess = (data) =>
         options.success = syncSuccess
+        options.error = syncError
+        event.trigger(@eventNames.LOCAL_SYNC_SUCCESS, data)
         Backbone.ajaxSync 'read', @, options
 
       @fetch
         success: fetchSuccess
+        error: (error) ->
+          event.trigger(@eventNames.LOCAL_SYNC_FAIL, error)
+
+      return event
 
     removeGarbage: () ->
       deferred = new $.Deferred()
@@ -66,7 +122,7 @@
       , options)
       do deferred.promise
 
-    getDelayedData: (status) ->
+    _getDelayedData: (status) ->
       deferred = new $.Deferred()
       data = []
       keyRange = @indexedDB.makeKeyRange
@@ -77,23 +133,67 @@
         keyRange: keyRange
         onEnd: () ->
           deferred.resolve(data)
-
       @indexedDB.iterate((item) ->
         data.push(item)
       , options)
-      do deferred.promise
+      deferred.promise()
+
+    getDelayedData: () ->
+      deferred = new $.Deferred()
+      deleted = @_getDelayedData(@states.DELETE_FAILED)
+      created = @_getDelayedData(@states.CREATE_FAILED)
+      updated = @_getDelayedData(@states.UPDATE_FAILED)
+      $.when(deleted, created,updated).done((a, b, c) ->
+        deferred.resolve(_.union(a, b, c))
+      )
+      deferred.promise()
+
+    fullSync: () ->
+      deferred = new $.Deferred()
+      @getDelayedData().done((delayedData)=>
+        console.log CONSOLE_TAG, 'fullsync', delayedData
+        count = 0
+        done = () =>
+          count++
+          if count is delayedData.length
+            Backbone.ajaxSync('read', @, success: (response) =>
+              data = @parse(response)
+              @set(data, silent: yes)
+              @markAsSynchronizing(delayedData).done(@removeGarbage().done(->deferred.resolve()))
+            , error: -> deferred.reject())
+
+        _.each(delayedData, (item) =>
+          method = @getSyncMethodsByState(item.status)
+          delete item.status
+          model = new @model(item)
+          console.log CONSOLE_TAG, 'full sync model', item, method
+          model.url = model.getUrlForSync(_.result(@, 'url'), method)
+
+          Backbone.ajaxSync(method, model, success: done, error: done)
+        )
+      )
+
+      deferred.promise()
 
     save: () ->
       deferred = new $.Deferred()
       @indexedDB.saveAll((-> deferred.resolve()), (-> deferred.reject()))
       do deferred.promise
 
-    markAsSynchronizing: () ->
-      models = @filter((item) ->
-        do item.isDelayed
-      )
-      models = _.map(models, (model) =>
-        model.set 'status': @states.SYNCHRONIZING
-        model.save()
-      )
-      $.when.apply $, models
+    markAsSynchronizing: (delayedData) ->
+      deferred = new $.Deferred()
+
+      map = (item) =>
+        item.status = @states.SYNCHRONIZING
+        deferred = new $.Deferred()
+        @indexedDB.store.put(item, (-> deferred.resolve()), (->deferred.reject()))
+        deferred.promise()
+
+      console.log CONSOLE_TAG, 'start to mark as synchronizing', delayedData
+      deferreds = _.map(delayedData, map)
+      $.when.apply($, deferreds).done(->
+        console.log CONSOLE_TAG, 'mark successful', delayedData
+        deferred.resolve(delayedData)
+      ).fail(->console.warn CONSOLE_TAG, 'could not mark as sync', arguments)
+
+      deferred.promise()
--- backbone.dualstorage.js
+++ backbone.dualstorage.js
@@ -8,6 +8,7 @@
     SYNCHRONIZED: 'SYNCHRONIZED',
     SYNCHRONIZING: 'SYNCHRONIZING',
     UPDATE_FAILED: 'UPDATE_FAILED',
+    CREATE_FAILED: 'CREATE_FAILED',
     DELETE_FAILED: 'DELETE_FAILED'
   };
 
@@ -32,28 +33,98 @@
 
   Backbone.DualModel = Backbone.Model.extend({
     states: states,
+    remoteIdAttribute: 'id',
+    getUrlForSync: function(urlRoot, method) {
+      var remoteId;
+      remoteId = this.get(this.remoteIdAttribute);
+      if (remoteId && (method === 'update' || method === 'delete')) {
+        return "" + urlRoot + "/" + remoteId + "/";
+      }
+      return urlRoot;
+    },
+    isInSynchronizing: function() {
+      return this.get('status') === this.states.SYNCHRONIZING;
+    },
     isDelayed: function() {
       var _ref;
       return (_ref = this.get('status')) === this.states.DELETE_FAILED || _ref === this.states.UPDATE_FAILED;
     }
   });
 
+  Backbone.IndexedDB.prototype.create = function(model, options) {
+    var data;
+    model.set('status', states.CREATE_FAILED);
+    data = model.attributes;
+    return this.store.put(data, (function(_this) {
+      return function(insertedId) {
+        data[_this.keyPath] = insertedId;
+        return options.success(data);
+      };
+    })(this), options.error);
+  };
+
+  Backbone.IndexedDB.prototype.update = function(model, options) {
+    var data;
+    model.set('status', states.UPDATE_FAILED);
+    data = model.attributes;
+    return this.store.put(data, options.success, options.error);
+  };
+
+  Backbone.IndexedDB.prototype.getAll = function(options) {
+    var data;
+    data = [];
+    return this.iterate(function(item) {
+      if (item.status !== states.DELETE_FAILED) {
+        return data.push(item);
+      }
+    }, {
+      onEnd: function() {
+        return options.success(data);
+      }
+    });
+  };
+
+  Backbone.IndexedDB.prototype.destroy = function(model, options) {
+    var data;
+    if (model.isNew()) {
+      return false;
+    }
+    model.set('status', states.DELETE_FAILED);
+    data = model.attributes;
+    return this.store.put(data, options.success, options.error);
+  };
+
   Backbone.DualCollection = Backbone.Collection.extend({
     states: states,
     eventNames: eventNames,
+    getSyncMethodsByState: function(state) {
+      var method;
+      return method = (function() {
+        switch (false) {
+          case this.states.CREATE_FAILED !== state:
+            return 'create';
+          case this.states.UPDATE_FAILED !== state:
+            return 'update';
+          case this.states.DELETE_FAILED !== state:
+            return 'delete';
+        }
+      }).call(this);
+    },
     merge: function(newData) {
       return newData;
     },
     firstSync: function(options) {
-      var fetchSuccess, originalSuccess, syncSuccess;
+      var event, fetchSuccess, originalSuccess, syncError, syncSuccess;
       if (options == null) {
         options = {};
       }
       originalSuccess = options.success || $.noop;
+      event = _.extend({}, Backbone.Events);
       syncSuccess = (function(_this) {
         return function(response) {
           var data, method;
           data = _this.merge(_this.parse(response));
+          event.trigger(_this.eventNames.REMOTE_SYNC_SUCCESS);
           method = options.reset ? 'reset' : 'set';
           _this[method](data, options);
           originalSuccess(_this, data, options);
@@ -61,15 +132,26 @@
           return wrapError(_this, options);
         };
       })(this);
+      syncError = (function(_this) {
+        return function(error) {
+          return event.trigger(_this.eventNames.REMOTE_SYNC_FAIL, error, options);
+        };
+      })(this);
       fetchSuccess = (function(_this) {
-        return function() {
+        return function(data) {
           options.success = syncSuccess;
+          options.error = syncError;
+          event.trigger(_this.eventNames.LOCAL_SYNC_SUCCESS, data);
           return Backbone.ajaxSync('read', _this, options);
         };
       })(this);
-      return this.fetch({
-        success: fetchSuccess
+      this.fetch({
+        success: fetchSuccess,
+        error: function(error) {
+          return event.trigger(this.eventNames.LOCAL_SYNC_FAIL, error);
+        }
       });
+      return event;
     },
     removeGarbage: function() {
       var deferred, idsForRemove, options, status;
@@ -94,7 +176,7 @@
       }, options);
       return deferred.promise();
     },
-    getDelayedData: function(status) {
+    _getDelayedData: function(status) {
       var data, deferred, keyRange, options;
       deferred = new $.Deferred();
       data = [];
@@ -114,6 +196,61 @@
       }, options);
       return deferred.promise();
     },
+    getDelayedData: function() {
+      var created, deferred, deleted, updated;
+      deferred = new $.Deferred();
+      deleted = this._getDelayedData(this.states.DELETE_FAILED);
+      created = this._getDelayedData(this.states.CREATE_FAILED);
+      updated = this._getDelayedData(this.states.UPDATE_FAILED);
+      $.when(deleted, created, updated).done(function(a, b, c) {
+        return deferred.resolve(_.union(a, b, c));
+      });
+      return deferred.promise();
+    },
+    fullSync: function() {
+      var deferred;
+      deferred = new $.Deferred();
+      this.getDelayedData().done((function(_this) {
+        return function(delayedData) {
+          var count, done;
+          console.log(CONSOLE_TAG, 'fullsync', delayedData);
+          count = 0;
+          done = function() {
+            count++;
+            if (count === delayedData.length) {
+              return Backbone.ajaxSync('read', _this, {
+                success: function(response) {
+                  var data;
+                  data = _this.parse(response);
+                  _this.set(data, {
+                    silent: true
+                  });
+                  return _this.markAsSynchronizing(delayedData).done(_this.removeGarbage().done(function() {
+                    return deferred.resolve();
+                  }));
+                },
+                error: function() {
+                  return deferred.reject();
+                }
+              });
+            }
+          };
+          return _.each(delayedData, function(item) {
+            var method, model;
+            method = _this.getSyncMethodsByState(item.status);
+            delete item.status;
+            model = new _this.model(item);
+            console.log(CONSOLE_TAG, 'full sync model', item, method);
+            model.url = model.getUrlForSync(_.result(_this, 'url'), method);
+            return Backbone.ajaxSync(method, model, {
+              success: done,
+              error: done
+            });
+          });
+        };
+      })(this));
+      return deferred.promise();
+    },
     save: function() {
       var deferred;
       deferred = new $.Deferred();
@@ -124,20 +261,30 @@
       }));
       return deferred.promise();
     },
-    markAsSynchronizing: function() {
-      var models;
-      models = this.filter(function(item) {
-        return item.isDelayed();
-      });
-      models = _.map(models, (function(_this) {
-        return function(model) {
-          model.set({
-            'status': _this.states.SYNCHRONIZING
-          });
-          return model.save();
+    markAsSynchronizing: function(delayedData) {
+      var deferred, deferreds, map;
+      deferred = new $.Deferred();
+      map = (function(_this) {
+        return function(item) {
+          item.status = _this.states.SYNCHRONIZING;
+          deferred = new $.Deferred();
+          _this.indexedDB.store.put(item, (function() {
+            return deferred.resolve();
+          }), (function() {
+            return deferred.reject();
+          }));
+          return deferred.promise();
         };
-      })(this));
-      return $.when.apply($, models);
+      })(this);
+      console.log(CONSOLE_TAG, 'start to mark as synchronizing', delayedData);
+      deferreds = _.map(delayedData, map);
+      $.when.apply($, deferreds).done(function() {
+        console.log(CONSOLE_TAG, 'mark successful', delayedData);
+        return deferred.resolve(delayedData);
+      }).fail(function() {
+        return console.warn(CONSOLE_TAG, 'could not mark as sync', arguments);
+      });
+      return deferred.promise();
     }
   });
 
